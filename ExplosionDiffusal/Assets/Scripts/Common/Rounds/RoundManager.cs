a8dc1d5c1355ab6ccc60956ae05810319974c310
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2023-05-02 12:02:36 +0200

    [0088]:[Mechanics][RoundTime]: new round popup

--- ExplosionDiffusal/Assets/Scripts/Common/Rounds/RoundManager.cs
+++ ExplosionDiffusal/Assets/Scripts/Common/Rounds/RoundManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 /// <summary>
@@ -9,19 +10,39 @@ public class RoundManager : MonoBehaviour
 {
     public static RoundManager instance;
 
+    [SerializeField] private Animator m_SwitchingSidesAnime;
+    [SerializeField] private Animator m_NewRoundAnime;
+    private TextMeshProUGUI m_RoundText;
+
     private int m_CurrentRound = 1;
     private int m_MaxRounds;
 
     private void Awake() => instance = this;
 
-    public void NewRound() { m_CurrentRound++; }
+    private void Start()
+    {
+        m_RoundText = m_NewRoundAnime.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void NewRound() { 
+        m_CurrentRound++;
+
+        if (IsHalfTime()) // TODO
+        {
+            m_SwitchingSidesAnime.Play("POPUP");
+        }
+
+        m_RoundText.text = string.Format("ROUND {0}", m_CurrentRound);
+        m_NewRoundAnime.Play("POPUP");
+    }
     public int GetCurrentRound() { return m_CurrentRound; }
 
     public void SetMaxRounds(int maxRounds) { m_MaxRounds = maxRounds; }
 
-    public bool IsHalfTime()
+    private bool IsHalfTime()
     {
         return m_CurrentRound == m_MaxRounds / 2;
     }
+
 
 }
