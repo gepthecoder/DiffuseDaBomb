84aabf0f67670ba2e7436bcf6683b3c1226cf625
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2023-05-21 12:27:58 +0200

    [0117]:[App][IAP]: import neon glow

---
+++ ExplosionDiffusal/Assets/Scripts/IAP/IAPManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum AdState { Disabled, Enabled }
+
+
+[System.Serializable]
+public class NoADS
+{
+    [SerializeField] private GameObject AdsNotBoughtUI;
+    [SerializeField] private GameObject AdsBoughtUI;
+
+    public void EnableAdsByState(AdState state) {
+        switch (state)
+        {
+            case AdState.Disabled:
+                AdsNotBoughtUI.SetActive(false);
+                AdsBoughtUI.SetActive(true);
+                break;
+            case AdState.Enabled:
+                AdsBoughtUI.SetActive(false);
+                AdsNotBoughtUI.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+public class IAPManager : MonoBehaviour
+{
+    public static IAPManager INSTANCE;
+
+    #region MONO
+    private void Awake()
+    {
+        if(INSTANCE == null)
+        {
+            INSTANCE = this;
+        } else { Destroy(this); }
+
+
+        if(PlayerPrefs.HasKey(_AdsPrefsID_))
+        {
+            // WE HAD A PREVIOUS SESSION
+            _AdsEnabled_ = PlayerPrefs.GetInt(_AdsPrefsID_, (int)AdState.Enabled);
+        } else
+        {
+            // SAVE
+            PlayerPrefs.SetInt(_AdsPrefsID_, _AdsEnabled_);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void Start()
+    {
+        m_NoADS?.EnableAdsByState((AdState)_AdsEnabled_);
+    }
+    #endregion
+
+    #region IAP
+    [Header("IAP")]
+    [SerializeField] private Animator m_IAPAnimator;
+    
+    public void ShowIAP() {
+        m_IAPAnimator.SetTrigger("show");
+    }
+
+    public void HideIAP() {
+        m_IAPAnimator.SetTrigger("hide");
+    }
+    #endregion
+
+    #region ADS
+    [SerializeField] private NoADS m_NoADS;
+
+    // 1 - enabled
+    // 0 - disabled
+    protected int _AdsEnabled_ = (int)AdState.Enabled;
+    protected readonly string _AdsPrefsID_ = "ADS";
+
+    public void OnNoADSPurchaseSuccessfulAction()
+    {
+        _AdsEnabled_ = (int)AdState.Disabled;
+
+        // SAVE
+        PlayerPrefs.SetInt(_AdsPrefsID_, _AdsEnabled_);
+        PlayerPrefs.Save();
+
+        // UPDATE VIEW
+        m_NoADS.EnableAdsByState((AdState)_AdsEnabled_);
+    }
+
+    #endregion
+}
