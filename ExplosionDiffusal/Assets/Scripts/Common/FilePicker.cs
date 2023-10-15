1412f47f63b987e049aa3bb290dc15b8eecdd14c
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2023-10-15 13:18:14 +0200

    [0153]:[App]:[Native]: add native file picker lib

---
+++ ExplosionDiffusal/Assets/Scripts/Common/FilePicker.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FilePicker : MonoBehaviour
+{
+    private string m_FinalPath;
+
+    public void LoadFile()
+    {
+        string fileType = NativeFilePicker.ConvertExtensionToFileType("png,jpg,jpeg");
+
+        NativeFilePicker.Permission permission = NativeFilePicker.PickFile((path) => { 
+            if(path == null)
+            {
+                Debug.Log("FilePicker::Operation Canceled!");
+            }else
+            {
+                m_FinalPath = path;
+                Debug.Log($"FilePicker::Picked File: {m_FinalPath}");
+            }
+        }, new string[] { fileType } );
+    }
+
+    public void SaveFile()
+    {
+        string filePath = Path.Combine(Application.temporaryCachePath, "bomba.txt");
+        File.WriteAllText(filePath, "G3PRODUCTION");
+
+        // export
+        NativeFilePicker.Permission permission = NativeFilePicker.ExportFile(
+            filePath,
+            (success)=> {
+                Debug.Log($"FilePicker:: File Exported: {success}");
+            }
+        );
+    }
+}
